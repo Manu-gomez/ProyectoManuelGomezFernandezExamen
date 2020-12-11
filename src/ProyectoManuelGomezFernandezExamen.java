a8a6fd190415c78ef4aa771f9ac854d9a86a4348
Manu-gomez <75076957+Manu-gomez@users.noreply.github.com>
2020-12-11 13:46:01 +0100

    Update ProyectoManuelGomezFernandezExamen.java

--- src/ProyectoManuelGomezFernandezExamen.java
+++ src/ProyectoManuelGomezFernandezExamen.java
@@ -1,8 +1,8 @@
 
 public class ProyectoManuelGomezFernandezExamen {
-
+	//Hola esto es una prueba
 	public static void main(String[] args) {
-		System.out.println("BUENAS MUNDO");
+		System.out.println("Manuel Gómez Fernández");
 	}
 
 }
